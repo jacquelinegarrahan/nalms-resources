fba1f19850e968c705ffe5567b3248e373b7e822
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-04-22 16:52:14 -0700

    Attach to given console

---
+++ scripts/attach-console.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Create tmux sessions and launch alarm services
+#
+# Copyright @2021 SLAC National Accelerator Laboratory
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: create-window.sh config_name"
+  exit 0
+fi
+
+if [[ -z "$1" ]]; then
+  echo "Configuration name not provided."
+  echo "Usage: create-window.sh config_name"
+  exit 0
+fi
+
+config_name=$1
+
+if [[ $( tmux list-windows -t nalms 2>/dev/null | grep $config_name ) ]] ; then
+    tmux $TMUX_OPTS attach -t nalms
+    tmux select-window -t nalms:$config_name
+else 
+    echo "No tmux window found for $config_name."
+fi
